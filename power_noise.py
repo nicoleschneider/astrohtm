f9eef6d7751aa30bc91e0efafa5d2bd6209e71f4
nicoleschneider <nschneider@loyola.edu>
2019-11-27 10:14:05 +0100

    commented and cleaned but still complex output

--- power_noise.py
+++ power_noise.py
@@ -26,18 +26,11 @@ from random import gauss
 
 import math
 import csv
-#----------------------------------------------------------------------
-# This function adjusts matplotlib settings for a uniform feel in the textbook.
-# Note that with usetex=True, fonts are rendered with LaTeX.  This may
-# result in an error if LaTeX is not installed on your system.  In that case,
-# you can set usetex to False.
-if "setup_text_plots" not in globals():
-    from astroML.plotting import setup_text_plots
-setup_text_plots(fontsize=8, usetex=True)
 
 
 # Open data
-FITS = fits.open('./srcB_3to40_cl_barycorr_binned_multiD.fits')
+SOURCEFILE = './srcB_3to40_cl_barycorr_binned_multiD.fits'
+FITS = fits.open(SOURCEFILE)
 
 data = astropy.table.Table.read(FITS[1])
 data = FITS[1].data
@@ -45,11 +38,12 @@ DATA_SIZE = len(data)
 print("LENGTH: ", DATA_SIZE)
 
 t_obs = data.field(0)
+# data.field(1) is the image which we ignore
 col = data.field(2)
-t_obs = t_obs[:-1]
+t_obs = t_obs[:-1]  # need even number of datapoints
 
 x_obs = col[:,0]
-x_obs = x_obs[:-1]
+x_obs = x_obs[:-1]  # need even number of datapoints
 
 
 # Make Power Spectrum
@@ -69,8 +63,9 @@ for i, beta in enumerate([1.0, 2.0]):
     print("F looks like: ", f[:20])
     print("PSD looks like: ", PSD[:20])
 
-
+#############################
 # Timmer and Konig Algorithm:
+
 seed(42)
 generated_f_pts = []
 
@@ -84,9 +79,10 @@ for w_i in range(len(f)):
     generated_f_pts.append(complex(real_part, imaginary_part))
   
 
+# Take FFT of original data
+FFT = fft(x_obs)  
 
-FFT = fft(x_obs)  # take FFT of original data
-
+# Choose power noise if gap, otherwise use original signal
 final_f_signal = []
 
 for i, f_pt in enumerate(FFT):
@@ -95,24 +91,23 @@ for i, f_pt in enumerate(FFT):
     else:
         final_f_signal.append(FFT[i])
 
-
-final_f_signal = [x if x.imag >=0 else conjugate(x) for x in final_f_signal]  # fix negative iamginary parts
+# Fix signal so inverse will be all real by taking conjugate
+final_f_signal = [x if x.imag >=0 else conjugate(x) for x in final_f_signal]  
 print("final signal looks like: ", final_f_signal[:20])
 
-sig = final_f_signal#ifft(final_f_signal)  # reverse the FFT to get back time series data
+# Reverse the FFT to get back time series data
+sig = ifft(final_f_signal)  
 print("Got back a signal of size: ", len(sig))
 
 
-
 # Output data to csv file
-l = [('t_obs', 'x_obs', 'final signal'), (t_obs, x_obs, sig)]
+headers = ('t_obs', 'x_obs', 'final signal')
+FILENAME = 'psd1.csv'
 
-with open('psd1.csv', 'w', ) as myfile:
+with open(FILENAME, 'w', ) as myfile:
     wr = csv.writer(myfile)
-#    zipped_list = zip(*l)
-    wr.writerow(l[0])
+    wr.writerow(headers)
     for i in range(len(sig)):
         wr.writerow([t_obs[i], x_obs[i], math.sqrt((sig[i].real)**2 + (sig[i].imag)**2)])
+		
 print("Wrote data to file")
-
-
