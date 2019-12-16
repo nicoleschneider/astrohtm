17705955aece03508117090673b8de82a82753af
nicoleschneider <nschneider@loyola.edu>
2019-12-16 16:22:30 +0100

    OO and added Optuna with 1 trial

--- spectrum_anomaly.py
+++ spectrum_anomaly.py
@@ -30,6 +30,7 @@ import csv
 import datetime
 import logging
 import tqdm
+import optuna
 import numpy as np
 import pandas as pd
 
@@ -46,162 +47,181 @@ from astropy.io import fits
 from astropy.io import ascii
 
 
+class AstroTest(object):
 
-_LOGGER = logging.getLogger(__name__)
+	_LOGGER = logging.getLogger(__name__)
+	_SOURCE_FILE = './srcB_3to40_cl_barycorr_binned_multiD.fits'
+	_OUTPUT_PATH = "spectrum5.csv"
+  
+	_MIN_VARIANCE = 0
+	_ANOMALY_THRESHOLD = 0.5
+	_ANOMALY_SCALE_FACTOR = 300
+	_SELECT_COLS = True
 
-FITS = fits.open('./srcB_3to40_cl_barycorr_binned_multiD.fits')
-data = astropy.table.Table.read(FITS[1])
-data = FITS[1].data
-DATA_SIZE = len(data)
-print("LENGTH: ", DATA_SIZE)
+	anomaly_count = 0
 
-headers = ['timestamp', 'b0', 'b1', 'b2', 'b3','b4','b5', 'b6', 'b7', 'b8', 'b9', 'b10', 'b11', 'b12', 'b13', 
-        'b14', 'b15', 'b16', 'b17', 'b18', 'b19', 'b20', 'b21', 'b22', 'b23', 'b24', 'b25', 'b26', 'b27', 
-        'b28', 'b29']
+	# minimum metric value of test_data.flc
+	_INPUT_MIN = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0 ,0 ,0]  # changed to min flux value
 
-_OUTPUT_PATH = "spectrum5.csv"  # changed name of output file
+	# maximum metric value of test_data.flc
+	_INPUT_MAX = [439, 140, 41, 17, 12, 9, 9, 8, 11, 9, 10, 7, 6, 6, 7, 5, 7, 5, 7, 6, 6, 5, 5, 6, 5, 6, 4, 5, 5, 5] # changed to max flux value
+	_INPUT_MAX = [300]*30
 
-_ANOMALY_THRESHOLD = 0.5
+	def __init__(self, min_var):
+		self._MIN_VARIANCE = min_var
 
-# minimum metric value of test_data.flc
-_INPUT_MIN = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0 ,0 ,0]  # changed to min flux value
+		self.hdu_list = fits.open(self._SOURCE_FILE)
+		self.data = astropy.table.Table.read(self.hdu_list[1])
+		self.data_size = len(self.data)
+		self.headers = ['timestamp', 'b0', 'b1', 'b2', 'b3','b4','b5', 'b6', 'b7', 'b8', 'b9', 'b10', 'b11', 'b12', 'b13', 
+			'b14', 'b15', 'b16', 'b17', 'b18', 'b19', 'b20', 'b21', 'b22', 'b23', 'b24', 'b25', 'b26', 'b27', 
+			'b28', 'b29']
+			
+		#self.model = self.createModel()
 
-# maximum metric value of test_data.flc
-_INPUT_MAX = [439, 140, 41, 17, 12, 9, 9, 8, 11, 9, 10, 7, 6, 6, 7, 5, 7, 5, 7, 6, 6, 5, 5, 6, 5, 6, 4, 5, 5, 5] # changed to max flux value
-_INPUT_MAX = [300]*30
+	def get_anomaly_count(self):
+		return self.anomaly_count
 
+	def _setRandomEncoderResolution(self, minResolution=0.001):
+		"""
+		Given model params, figure out the correct resolution for the
+		RandomDistributed encoders. Modifies params in place.
+		"""
+		fields = self.headers[1:]
+		print(fields)
 
-MIN_VARIANCE = 240
-ANOMALY_SCALE_FACTOR = 300
+		for i, field in enumerate(fields):
+			encoder = (model_params.MODEL_PARAMS["modelParams"]["sensorParams"]["encoders"][field])
 
-def _setRandomEncoderResolution(minResolution=0.001):
-  """
-  Given model params, figure out the correct resolution for the
-  RandomDistributed encoders. Modifies params in place.
-"""
+		if encoder["type"] == "RandomDistributedScalarEncoder":
+			rangePadding = abs(self._INPUT_MAX[i] - self._INPUT_MIN[i]) * 0.2
+			minValue = self._INPUT_MIN[i] - rangePadding
+			maxValue = self._INPUT_MAX[i] + rangePadding
+			resolution = max(minResolution, (maxValue - minValue) / encoder.pop("numBuckets") )
+			encoder["resolution"] = resolution
 
-  fields = headers[1:]
-  print(fields)
+	def createModel(self):
+		self._setRandomEncoderResolution()
+		return ModelFactory.create(model_params.MODEL_PARAMS)
 
-  for i, field in enumerate(fields):
-    encoder = (
-      model_params.MODEL_PARAMS["modelParams"]["sensorParams"]["encoders"][field]
-    )
+	def select_cols(self, col):
+		if self._SELECT_COLS:
 
-    if encoder["type"] == "RandomDistributedScalarEncoder":
-      rangePadding = abs(_INPUT_MAX[i] - _INPUT_MIN[i]) * 0.2
-      minValue = _INPUT_MIN[i] - rangePadding
-      maxValue = _INPUT_MAX[i] + rangePadding
-      resolution = max(minResolution,
-                       (maxValue - minValue) / encoder.pop("numBuckets")
-                      )
-      encoder["resolution"] = resolution
+			for i, element in enumerate(self.headers[1:]):    
+				col[:,i] =  col[:,i] - np.mean(col[:,i]) #/( np.std(col[:,i]) )
+			#  _INPUT_MAX[i] =  _INPUT_MAX[i] - np.mean(col[:,i]) #/( np.std(col[:,i]) )
+				col[:,i] = map(lambda x: max(x,0), col[:,i])
 
+				if np.var(col[:,i]) < self._MIN_VARIANCE:
+					self.headers.remove(element)
+					model_params.MODEL_PARAMS["modelParams"]["sensorParams"]["encoders"].pop(element)
+			  
+		 # _INPUT_MAX = map(lambda x: max(x,0), _INPUT_MAX)	  
+			print(self._INPUT_MAX)
+			print("ENDED UP USING ", len(self.headers), " columns total")
+		  
+		return col
+		
+		
+	def replace_bad_intervals(self, col):
+		df = pd.read_csv("psd1.csv")
+		saved_column = df['final signal'].values
+		saved_column = np.append(saved_column, [0])
+		col[:,0] = saved_column
 
-def createModel():
-  _setRandomEncoderResolution()
-  return ModelFactory.create(model_params.MODEL_PARAMS)
+		print col
+		return col
+  
+	def preprocess(self, col):
+		col = self.select_cols(col)
+		col = self.replace_bad_intervals(col)
+		return col
+  
+	def extract_cols_from_data(self):
+		col0 = self.data.field(0)
+		# self.data.field(1) is the image which we will ignore for now
+		col = self.data.field(2)
+		col = self.preprocess(col)
+		return col0, col
+  
+	def setup_output(self):
+		shifter = InferenceShifter()
+		output = nupic_anomaly_output.NuPICFileOutput("astronomy-data")
+		f = open(self._OUTPUT_PATH,"wb")
+		csvWriter = csv.writer(f)
+		csvWriter.writerow(["timestamp", "b0", "b1", "scaled_score", "anomaly_score"])
+		return f, output, csvWriter
 
-def select_cols(col):
-  global headers
-#  global _INPUT_MAX
+	def generate_record(self, bs, col0, col, index):
+		record = []
+		for x, label in enumerate(bs):
+			col_number = int(label[1:])
+			record.append(col[:,col_number][index]) 
+	
+		record = np.insert(record, 0, col0[index]-col0[0]) # insert timestamp value to front of record
+		return record
+  
+	def generate_model_input(self, col0, col, index):
+		bs = self.headers[1:]
+		record = self.generate_record(bs, col0, col, index)
+		modelInput = dict(zip(self.headers, record))
+	
+		for b in bs:
+			modelInput[b] = float(modelInput[b])
+	
+		floattime = float(modelInput['timestamp'])
+		modelInput["timestamp"] = datetime.datetime.fromtimestamp(floattime)
+		return modelInput
 
-  for i, element in enumerate(headers[1:]):    
-    col[:,i] =  col[:,i] - np.mean(col[:,i]) #/( np.std(col[:,i]) )
-  #  _INPUT_MAX[i] =  _INPUT_MAX[i] - np.mean(col[:,i]) #/( np.std(col[:,i]) )
-    col[:,i] = map(lambda x: max(x,0), col[:,i])
-
-
-    if np.var(col[:,i]) < MIN_VARIANCE:
-      headers.remove(element)
-      model_params.MODEL_PARAMS["modelParams"]["sensorParams"]["encoders"].pop(element)
+	def run_model(self, modelInput):
+		result = self.model.run(modelInput)
+		anomalyScore = result.inferences['anomalyScore']
+		scaledScore = anomalyScore * self._ANOMALY_SCALE_FACTOR
+		return anomalyScore, scaledScore
+	
+	def output_results(self, output, csvWriter, modelInput, anomalyScore, scaledScore):
+		output.write(modelInput['timestamp'], modelInput['b0'], 0, anomalyScore)
 	  
- # _INPUT_MAX = map(lambda x: max(x,0), _INPUT_MAX)	  
+		if anomalyScore > self._ANOMALY_THRESHOLD:
+			self.anomaly_count = self.anomaly_count + 1
+			self._LOGGER.info("Anomaly detected at [%s]. Anomaly score: %f.", modelInput["timestamp"], anomalyScore)
+		csvWriter.writerow([modelInput["timestamp"], modelInput["b0"], scaledScore, "%.3f" % anomalyScore])
 
-  print(_INPUT_MAX)
-  print("ENDED UP USING ", len(headers), " columns total")
-  return col
-
-
-def replace_bad_intervals(col):
-  df = pd.read_csv("psd1.csv")
-  saved_column = df['final signal'].values
-  saved_column = np.append(saved_column, [0])
-  col[:,0] = saved_column
-
-  print col
-  return col
-  
-def preprocess(col):
-  col = select_cols(col)
-  col = replace_bad_intervals(col)
-  return col
-  
-def extract_cols_from_data(data):
-  col0 = data.field(0)
-  # data.field(1) is the image which we will ignore for now
-  col = data.field(2)
-  col = preprocess(col)
-  return col0, col
-  
-def setup_output():
-  shifter = InferenceShifter()
-  output = nupic_anomaly_output.NuPICFileOutput("astronomy-data")
-  f = open(_OUTPUT_PATH,"wb")
-  csvWriter = csv.writer(f)
-  csvWriter.writerow(["timestamp", "b0", "b1", "scaled_score", "anomaly_score"])
-  return f, output, csvWriter
-
-def generate_model_input(col0, col, index):
-  bs = headers[1:]
-  record = []
-  for x, label in enumerate(bs):
-    col_number = int(label[1:])
-    record.append(col[:,col_number][index]) 
+	def close_output(self, f, output):
+		print("Anomaly Scores have been written to", self._OUTPUT_PATH)
+		output.close()
+		f.close()
 	
-  record = np.insert(record, 0, col0[index]-col0[0]) # insert timestamp value to front of record
-  modelInput = dict(zip(headers, record))
-	
-  for b in bs:
-    modelInput[b] = float(modelInput[b])
-	
-  floattime = float(modelInput['timestamp'])
-  modelInput["timestamp"] = datetime.datetime.fromtimestamp(floattime)
-  return modelInput
+	def runAstroAnomaly(self):
+		print("Running with min var of: ", self._MIN_VARIANCE)
+		f, output, csvWriter = self.setup_output()
+		col0, col = self.extract_cols_from_data()
   
-def run_model(model, modelInput):
-    result = model.run(modelInput)
-    anomalyScore = result.inferences['anomalyScore']
-    scaledScore = anomalyScore * ANOMALY_SCALE_FACTOR
-    return anomalyScore, scaledScore
-	
-def output_results(output, csvWriter, modelInput, anomalyScore, scaledScore):
-  output.write(modelInput['timestamp'], modelInput['b0'], 0, anomalyScore)
-  
-  if anomalyScore > _ANOMALY_THRESHOLD:
-    _LOGGER.info("Anomaly detected at [%s]. Anomaly score: %f.", modelInput["timestamp"], anomalyScore)
-  csvWriter.writerow([modelInput["timestamp"], modelInput["b0"], scaledScore, "%.3f" % anomalyScore])
-
-def close_output(f, output):
-  print("Anomaly Scores have been written to",_OUTPUT_PATH)
-  output.close()
-  f.close()
-	
-def runAstroAnomaly():
-  f, output, csvWriter = setup_output()
-  col0, col = extract_cols_from_data(data)
-  
-  model = createModel()
-  model.enableInference({'predictedField': 'b0'})  # doesn't matter for anomaly detection
-  
-  for i in tqdm.tqdm(range(0, DATA_SIZE, 1), desc='% Complete'):
-    modelInput = generate_model_input(col0, col, i)
-    anomalyScore, scaledScore = run_model(model, modelInput)
-    output_results(output, csvWriter, modelInput, anomalyScore, scaledScore)
+		self.model = self.createModel()
+		self.model.enableInference({'predictedField': 'b0'})  # doesn't matter for anomaly detection
+	  
+		for i in tqdm.tqdm(range(0, self.data_size, 1), desc='% Complete'):
+			modelInput = self.generate_model_input(col0, col, i)
+			anomalyScore, scaledScore = self.run_model(modelInput)
+			self.output_results(output, csvWriter, modelInput, anomalyScore, scaledScore)
     
-  close_output(f, output)
+		self.close_output(f, output)
+		
+  
 
+
+	def objective(self, trial):
+		self._MIN_VARIANCE = trial.suggest_int('_MIN_VARIANCE', 239, 241)
+		self.runAstroAnomaly()
+		return -1 * self.get_anomaly_count()
 
 if __name__ == "__main__":
-  logging.basicConfig(level=logging.INFO)
-  runAstroAnomaly()
+	logging.basicConfig(level=logging.INFO)
+	
+	astro_test = AstroTest(233)
+	print(astro_test._MIN_VARIANCE)
+	
+	study = optuna.create_study()
+	study.optimize(astro_test.objective, n_trials=1)
+
+	
