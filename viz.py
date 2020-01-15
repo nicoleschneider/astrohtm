2bb6f92c60fc82beee1baecb7e188b1a6e52e355
nicoleschneider <nschneider@loyola.edu>
2020-01-15 10:25:58 +0100

    customization of timestamp range and which spectra

--- viz.py
+++ viz.py
@@ -8,8 +8,21 @@ import numpy as np
 
 input_filename = 'nu80002092008A01_x2_bary_binned10.csv'
 output_filename = 'spectra.png'
-df = read_csv(input_filename)
+_MIN = 1000
+_MAX = 1500
 
+def trim_timestamp(xs, ys, min, max):
+	xs = xs[min:max]
+	ys = ys[min:max]
+	return xs, ys
+
+def choose_spectra(df, start, fin):
+	labels = ['b' + str(x) for x in range(start, fin+1)]
+	return df.drop(labels, axis='columns')
+
+
+df = read_csv(input_filename)
+df = choose_spectra(df, 0, 10)
 print "size:", len(df.columns)
 
 fig, axs = plt.subplots(len(df.columns)-1, sharex=True, sharey=True)
@@ -18,7 +31,9 @@ fig.suptitle('Spectra')
 for i in range(len(df.columns)-1):
 	xs = np.array(df['timestamp'])
 	ys = np.array(df[df.columns[1::1]])[:,i]
+	xs, ys = trim_timestamp(xs, ys, _MIN, _MAX)
 	axs[i].plot(xs, ys)
+	
 	
 
 for ax in axs:
