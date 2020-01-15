fca1808e5b68f06a8b3e87a4a7a73cfa2981e90d
nicoleschneider <nschneider@loyola.edu>
2020-01-15 10:00:56 +0100

    makes nice spectra now

--- viz.py
+++ viz.py
@@ -10,21 +10,24 @@ input_filename = 'nu80002092008A01_x2_bary_binned10.csv'
 output_filename = 'spectra.png'
 df = read_csv(input_filename)
 
-print(df)
-print("size: ", len(df.columns))
+print "size:", len(df.columns)
+
+fig, axs = plt.subplots(len(df.columns)-1, sharex=True, sharey=True)
+fig.suptitle('Spectra')
 
 for i in range(len(df.columns)-1):
-	xs = np.array(df['timestamp']) 
+	xs = np.array(df['timestamp'])
 	ys = np.array(df[df.columns[1::1]])[:,i]
-	plt.subplot(len(df.columns)-1, 1, i+1)
-	plt.plot(xs, ys)
+	axs[i].plot(xs, ys)
+	
+
+for ax in axs:
+	ax.label_outer()
+	ax.get_yaxis().set_visible(False)
 
 
-plt.xlabel('Timestamp')
-plt.ylabel('Photon Count')
-
-plt.grid(alpha=0.1)    
-plt.yscale('log')
-
+plt.xlabel('Timestamp')    
 plt.show()
 plt.savefig(output_filename)
+
+print "Plot was saved to", output_filename
